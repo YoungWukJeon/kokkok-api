4e1b91a534df1bfb508259f1242f8a8df1c222ff
YoungWukJeon <kdh10080@gmail.com>
2020-08-18 10:10:37 +0900

    initial commit

---
+++ src/main/java/com/study/kokkokapi/KokkokApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.kokkokapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokkokApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokkokApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/kokkokapi/KokkokApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.kokkokapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokkokApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
